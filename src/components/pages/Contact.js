18b58627cf7284c2ae1c273d42ff56793166fc2e
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-11-11 22:05:40 -0800

    fixed input form to gather data

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,31 @@
-import React from "react";
-
+import React, {useState} from "react";
+import axios from "axios";
 function Contact() {
+
+    const [contact, setContact] = useState({
+        name:"",
+        email:"",
+        message:""
+    })
+    function handleFormSubmit(event){
+        event.preventDefault();
+        setContact({
+            name:"",
+            email:"",
+            message:""
+        })
+      
+
+    };
+
+    function handleInputChange (event){
+        let key = event.target.name;
+        console.log(event.target.name)
+        let value = event.target.value;
+        setContact({...contact,[key]:value})
+    }
+    
+    console.log(contact);
     return (
         <div className="container">
             <div className="row pt-2"><br />
@@ -12,29 +37,29 @@ function Contact() {
             {/* <!-- Contact info starts here --> */}
             <main className="row justify-content-center mt-3 pl-0">
                 <div className="col-md-8">
-                    <form className="bg-dark p-2">
+                    <form className="bg-dark p-2" >
 
                         {/* <!-- Name input --> */}
                         <div className="form-group">
                             <label className="text-info" >Name</label>
-                            <input className="form-control" placeholder="First Last" />
+                            <input className="form-control" placeholder="First Last" name="name" value={contact.name} onChange={handleInputChange}/>
                             <br />
                         </div>
 
                         {/* <!-- Email input --> */}
                         <div className="form-group">
                             <label className="text-info" htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email"  name="email" value={contact.email} onChange={handleInputChange}/>
                             <br />
                         </div>
 
                         {/* <!-- Message input --> */}
                         <div className="form-group">
                             <label className="text-info" htmlFor="comment">Message</label>
-                            <textarea className="form-control" rows="5" id="comment" placeholder="Message"></textarea>
+                            <textarea className="form-control" rows="5" id="comment" placeholder="Message"  name="message" value={contact.message} onChange={handleInputChange}></textarea>
                             <br />
                         </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary" onSubmit={handleFormSubmit}>Submit</button>
                     </form>
                 </div>
 

959f6c5ee5eed6c9d61d090fb1757e40d11db3e9
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-11-11 16:57:36 -0800

    added contacts page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,8 +1,52 @@
 import React from "react";
 
-function Contact(){
+function Contact() {
     return (
-        <h1>Contact Page</h1>
+        <div className="container">
+            <div className="row pt-2"><br />
+
+                <div Class="col-md-3 border-bottom -info">
+                    <h1>Contact</h1>
+                </div>
+            </div>
+
+            {/* <!-- Contact info starts here --> */}
+            <main className="row justify-content-center mt-3 pl-0">
+                <div className="col-md-8">
+                    <form className="bg-dark p-2">
+
+                        {/* <!-- Name input --> */}
+                        <div className="form-group">
+                            <label className="text-info" >Name</label>
+                            <input className="form-control" placeholder="First Last" />
+                            <br />
+                        </div>
+
+                        {/* <!-- Email input --> */}
+                        <div className="form-group">
+                            <label className="text-info" for="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                            <br />
+                        </div>
+
+                        {/* <!-- Message input --> */}
+                        <div className="form-group">
+                            <label className="text-info" for="comment">Message</label>
+                            <textarea className="form-control" rows="5" id="comment" placeholder="Message"></textarea>
+                            <br />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+
+            </main>
+
+
+
+
+
+        </div>
+
     )
 };
 

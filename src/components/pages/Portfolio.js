0757ec42eabef7743103ac303749e3c02fd4f45a
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-12-12 20:23:06 -0800

    adjust background color viewheight, revisit portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -20,7 +20,7 @@ function Portfolio() {
                 </div>
                 {/* ================cards================================= */}
 
-                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 pb-5">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 pb-5" >
                     <Project
                         projectImg={PlantAhead}
                         imageAlt={"Plant Ahead Main Page"}

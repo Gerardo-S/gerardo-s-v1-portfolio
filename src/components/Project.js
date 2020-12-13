31b70df7372fdc079f4a8545a81f89354f269e70
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-12-12 20:14:18 -0800

    add technologies section to cards with shadow

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,10 @@
 import React from "react";
 // Work in progress, I feel it would be best to create a json file with listed properties of my projects to make this work as intended. I will revisit.
 function Project(props) {
-    const { projectImg, imageAlt, projectTitle, projectDescription, imgLink, repoLink } = props;
+    const { projectImg, imageAlt, projectTitle, projectDescription, imgLink, repoLink, techList } = props;
     return (
         <div className="col mb-4 ">
-            <div className="card h-100">
+            <div className="card h-100 shadow">
                 <a href={imgLink} rel="noreferrer" target="_blank">
                     <img src={projectImg} style={{ height: 180, objectFit: "cover" }} className="card-img-top border-bottom border-info border-4" alt={imageAlt} />
 
@@ -12,6 +12,10 @@ function Project(props) {
                 <div className="card-body">
                     <h5 className="card-title">{projectTitle}</h5>
                     <p className="card-text">{projectDescription}</p>
+                    <h6 className="card-title">Technologies Used</h6>
+                   <p style={{color:"#28ACEA"}}>
+                       {techList}
+                   </p>
                 </div>
                 <div className="card-footer" style={{
                     
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -32,6 +32,7 @@ function Portfolio() {
                         }
                         imgLink={"https://infinite-plains-30611.herokuapp.com/"}
                         repoLink={"https://github.com/Deftone1/plant-ahead"}
+                        techList={"React, JavaScript, Express, Bootstrap-React, React-Spring, MongoDB, Mongoose, Node-Fetch, Trefle"}
                     />
                     <Project
                         projectImg={GameKiwi}
@@ -41,6 +42,7 @@ function Portfolio() {
                         + " Members have the ability to save games to their top-ten list and share with other users."}
                         imgLink={"https://infinite-plains-30611.herokuapp.com/"}
                         repoLink={"https://github.com/Gerardo-S/bluetooth-coffee-cup"}
+                        techList={"Express, Express-Handlebars, Passport, and Sequelize"}
                     />
 
                     <Project
@@ -51,6 +53,7 @@ function Portfolio() {
                         " Each burger after submission is listed with a devour button which users click to devour burger."}
                         imgLink={"https://destroy-da-burger.herokuapp.com/"}
                         repoLink={"https://github.com/Gerardo-S/burger"}
+                        techList={"MySQL, Node, Express, Handlebars, ORM"}
                     />
 
                     <Project
@@ -61,6 +64,8 @@ function Portfolio() {
                          " This app offers users the ability to search for new recipes and cuisines."}
                         imgLink={"https://babydylan.github.io/recipe-roulette/"}
                         repoLink={"https://github.com/babydylan/recipe-roulette"}
+                        techList={"HTML, CSS, 2 API's"}
+                        
                     />
 
                     <Project
@@ -71,6 +76,7 @@ function Portfolio() {
                         " through a series of questions from the command line."}
                         imgLink={"https://github.com/Gerardo-S/README-Generator"}
                         repoLink={"https://github.com/Gerardo-S/README-Generator"}
+                        techList={"Inquirer, Node.js"}
                     />
 
                     <Project
@@ -80,6 +86,7 @@ function Portfolio() {
                         projectDescription={"Functional budget tracker with offline accessability."}
                         imgLink={"https://tracking-that-budget.herokuapp.com/"}
                         repoLink={"https://github.com/Gerardo-S/track-your-budget"}
+                        techList={"IndexedDb, Service-Worker, Progressive Web App"}
                     />
 
                     <Project
@@ -89,6 +96,7 @@ function Portfolio() {
                         projectDescription={"An app that lets users track their workouts."}
                         imgLink={"https://fitness-on-point.herokuapp.com/"}
                         repoLink={"https://github.com/Gerardo-S/fitness-tracker"}
+                        techList={"Mongoose, and Express"}
                     />
 
                     <Project
@@ -98,6 +106,7 @@ function Portfolio() {
                         projectDescription={"An app that generates an HTML webpage displaying a Team member roster."}
                         imgLink={"https://github.com/Gerardo-S/team-profile-generator"}
                         repoLink={"https://github.com/Gerardo-S/team-profile-generator"}
+                        techList={"Node.js, Inquirer, Jest"}
                     />
 
                     <Project
@@ -107,6 +116,7 @@ function Portfolio() {
                         projectDescription={"Displays a live five day forecast from city of choice."}
                         imgLink={"https://gerardo-s.github.io/5-Day_Forecast/"}
                         repoLink={"https://github.com/Gerardo-S/5-Day_Forecast"}
+                        techList={"HTML, CSS, OpenWeather API, Bootstrap"}
                     />
 
                     <Project
@@ -116,104 +126,13 @@ function Portfolio() {
                         projectDescription={"Generates passwords based on user criteria."}
                         imgLink={"https://gerardo-s.github.io/password-generator/"}
                         repoLink={"https://github.com/Gerardo-S/password-generator"}
+                        techList={"HTML, CSS"}
                     />
 
 
                 </div>
 
-
-                {/* ============================================ */}
-
-                {/* <div className=" row-md-12 border border-dark p-0 m-0 ">
-
-                    <div className="row bg-light m-3 row-cols ">
-                        <div className="col-md-4 p-1">
-                            <a href="https://plant-ahead.herokuapp.com/" rel="noreferrer" target="_blank">
-                                <img src={PlantAhead} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="Game-Kiwi-App" />
-                            </a>
-                            View Repo:<a href="https://github.com/Deftone1/plant-ahead" rel="noreferrer" target="_blank"> Plant Ahead</a>
-                        </div>
-
-                        <div className="col-md-4 p-1">
-                            <a href="https://infinite-plains-30611.herokuapp.com/" rel="noreferrer" target="_blank">
-                                <img src={GameKiwi} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="Game-Kiwi-App" />
-                            </a>
-                            View Repo:<a href="https://github.com/Gerardo-S/bluetooth-coffee-cup" rel="noreferrer" target="_blank"> Game Kiwi</a>
-                        </div>
-
-                        <div className="col-md-4 p-1">
-                            <a href="https://destroy-da-burger.herokuapp.com/" rel="noreferrer" target="_blank">
-                                <img src={DevourThatBurger} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="Eat-Da-Burger" />
-                            </a>
-                            View Repo:<a href="https://github.com/Gerardo-S/burger" rel="noreferrer" target="_blank"> Devour That Burger</a>
-                        </div>
-
-                    </div>
-
-                    <div className="row bg-light m-3 row-cols">
-
-
-                        <div className="col-md-4 p-1">
-                            <a href="https://babydylan.github.io/recipe-roulette/" rel="noreferrer" target="_blank">
-                                <img src={RecipeRoulette} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="Recipe Roulette" />
-                            </a>
-                            View Repo:<a href="https://github.com/babydylan/recipe-roulette" rel="noreferrer" target="_blank"> Recipe Roulette</a>
-                        </div>
-
-                        <div className="col-md-4 p-1">
-                            <a href="https://github.com/Gerardo-S/README-Generator" rel="noreferrer" target="_blank">
-                                <img src={ReadMeGenerator} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="placeholder" />
-                            </a>
-                            View Repo:<a href="https://github.com/Gerardo-S/README-Generator" rel="noreferrer" target="_blank"> ReadMe Generator With Inquirer</a>
-                        </div>
-
-                        <div className="col-md-4 p-1">
-                            <a href="https://tracking-that-budget.herokuapp.com/" rel="noreferrer" target="_blank">
-                                <img src={TrackThatBudget} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="placeholder" />
-                            </a>
-                            View Repo:<a href="https://github.com/Gerardo-S/track-your-budget" rel="noreferrer" target="_blank"> Track Your Budget</a>
-                        </div>
-                    </div>
-
-                    <div className="row bg-light m-3 ">
-                        <div className="col-md-4 p-1">
-                            <a href="https://fitness-on-point.herokuapp.com/" rel="noreferrer" target="_blank">
-                                <img src={FitnessTracker} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="placeholder" />
-                            </a>
-                            View Repo:<a href="https://github.com/Gerardo-S/fitness-tracker" rel="noreferrer" target="_blank"> Fitness Tracker</a>
-                        </div>
-
-                        <div className="col-md-4 p-1">
-                            <a href="https://github.com/Gerardo-S/team-profile-generator" rel="noreferrer" target="_blank">
-                                <img src={GeneratedTeam} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="placeholder" />
-                            </a>
-                            View Repo:<a href="https://github.com/Gerardo-S/team-profile-generator" rel="noreferrer" target="_blank"> Team Generator Using Node CLI</a>
-                        </div>
-
-                        <div className="col-md-4 p-1">
-                            <a href="https://gerardo-s.github.io/5-Day_Forecast/" rel="noreferrer" target="_blank">
-                                <img src={WeatherDashboard} className="img-fluid img-thumbnail" style={{ width: 360, height: 360 }} alt="placeholder" />
-                            </a>
-                            View Repo:<a href="https://github.com/Gerardo-S/5-Day_Forecast" rel="noreferrer" target="_blank"> Weather-Dashboard</a>
-                        </div>
-
-
-                    </div>
-
-                    <div className="row bg-light m-3">
-                        <div className="col-md-4 p-1">
-                            <a href="https://gerardo-s.github.io/password-generator/" rel="noreferrer" target="_blank">
-                                <img src={GeneratedPassword} className="img-fluid img-thumbnail" style={{ height: 360, objectFit: 'contain' }} alt="Password Generator" />
-                            </a>
-                            View Repo:<a href="https://github.com/Gerardo-S/password-generator" rel="noreferrer" target="_blank"> Password Generator</a>
-                        </div>                    
-
-                    </div>
-                    <br />
-                    <br />
-                </div> */}
-                <br />
-                <br />
+                
 
             </main>
         </div>

63ac1f98b29976718115b69f5dc47bd73e64c1e0
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-03-28 00:47:34 -0700

    Add Styling to 'About Page'  Summary:          Installed react-bootstrap to utilize collapsing navbar feature. Imported NavBar component to Nav.js file and moved content into NavBar div. Set collapsilbe          nav items into 'NavBar.Collapse' div wrapper. Set nav text style by modifying 'Nav.css' style sheet.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,43 +1,47 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import NavBar from "react-bootstrap/Navbar";
 import "../styles/Nav.css"
 function Nav() {
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-info sticky-top">
+        <NavBar collapseOnSelect expand="lg" className="navbar-light bg-info sticky-top text">
+
             <h1 className="navbar-brand name text-white">Gerardo Solis</h1>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+            <NavBar.Toggle aria-controls="responsive-navbar-nav" />
+            <NavBar.Collapse id="responsive-navbar-nav" >
+                <div className="navbar-collapse nav justify-content-end" id="navbarSupportedContent">
 
-            <div className="collapse navbar-collapse nav justify-content-end" id="navbarSupportedContent">
+                    <ul className="nav justify-content-end">
 
-                <ul className="nav justify-content-end">
+                        <li>
+                            <NavLink exact to="/contact" className="nav-link text-light" activeClassName="active">
+                                Contact
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/portfolio" className="nav-link text-light" activeClassName="active">
+                                Portfolio
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/about" className="nav-link text-light" activeClassName="active">
+                                About
+                            </NavLink>
 
-                    <li>
-                        <NavLink exact to="/contact" className="nav-link text-light" activeClassName="active">
-                            Contact
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to="/portfolio" className="nav-link text-light" activeClassName="active">
-                            Portfolio
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink exact to="/about" className="nav-link text-light" activeClassName="active">
-                            About
-                        </NavLink>
-                        
-                    </li>
+                        </li>
 
-                </ul>
+                    </ul>
 
-            </div>
+                </div>
+
+            </NavBar.Collapse>
+
+        </NavBar>
 
 
-        </nav>
+
+
     )
 };
 
